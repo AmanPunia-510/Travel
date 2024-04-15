3a72b1dc933c684eb8022bd54dc8a7fa712a7eec
AmanPunia-510 <puniaaman888@gmail.com>
2024-04-15 12:21:45 +0530

    project almost complete with preloader

--- assets/js/script.js
+++ assets/js/script.js
@@ -139,7 +139,7 @@ const preloader = document.querySelector(".preloader");
 
 setTimeout(() => {
   preloader.classList.add("d-none");
-}, 2000);
+}, 3000);
 
 let vid = document.querySelector(".video");
 vid.playbackRate = 0.5;
